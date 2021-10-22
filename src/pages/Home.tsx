b288bf7e36c27605d028bd1d340c3b089c6af319
Milan Haria <milan.haria@morrisonsplc.co.uk>
2021-10-22 10:59:13 +0100

    initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,33 @@
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Home.css";
+
+const Tab1: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse='condense'>
+          <IonToolbar>
+            <IonTitle size='large'>Home</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonButton expand='full' routerLink='/tab1'>
+          Enter
+        </IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
